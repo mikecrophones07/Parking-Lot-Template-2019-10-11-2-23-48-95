bf8c35a1e636ea74d29973a164c37e143f945951
Mike Anthony A. Echon <Mikecrophones07>
2019-10-20 16:23:39 +0800

    - add fields to be populate when creating an order

--- src/main/java/com/thoughtworks/parking_lot/Service/ParkingOrdersService.java
+++ src/main/java/com/thoughtworks/parking_lot/Service/ParkingOrdersService.java
@@ -6,7 +6,9 @@ import com.thoughtworks.parking_lot.Entity.ParkingOrders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 
@@ -22,9 +24,17 @@ public class ParkingOrdersService {
     public ParkingOrders save(String name, ParkingOrders parkingOrder) {
         ParkingLot fetchParkingLots = parkingLotService.getSpecificParkingLot(name);
         if(Objects.nonNull(fetchParkingLots)){
+            parkingOrder.setCreationTime(generateCurrentTime());
+            parkingOrder.setStatus(true);
             parkingOrder.setParkingLot(fetchParkingLots);
             return parkingOrderRepo.save(parkingOrder);
         }
         return null;
+    }
+
+    private String generateCurrentTime() {
+        Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        return formatter.format(date);
     }
 }
